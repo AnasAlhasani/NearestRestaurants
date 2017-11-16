a3a576b9fbd349a0e32404f97566c3ec0e3aa482
Anas Alhasani <anasalhassni@gmail.com>
2017-11-16 16:09:49 +0200

    added check for authroization

--- NearestRestaurants/Helpers/LocationClient.swift
+++ NearestRestaurants/Helpers/LocationClient.swift
@@ -40,6 +40,7 @@ class LocationClient: NSObject {
     }
     
     func startUpdatinglocation() {
+        guard isAuthorizedWhenInUse else { return }
         locationManager?.startUpdatingLocation()
     }
     
@@ -47,6 +48,14 @@ class LocationClient: NSObject {
         locationManager?.stopUpdatingLocation()
     }
     
+    
+    var isAuthorizedWhenInUse: Bool {
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            return true
+        }else {
+            return false
+        }
+    }
     
     var currentLatitude: String {
         return String(format: "%f",
